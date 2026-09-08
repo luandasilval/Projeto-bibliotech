2329e3eda1a3cf4462821b1c17b071b6958fb658
luandasilval <luanda.leite@edu.senai.br>
2026-09-08 07:58:17 -0300

    ✨ feat: Adicionando página de login, inlcuindo js para manipular a visualização de senha.

---
+++ Login/login.js
@@ -0,0 +1,22 @@
+
+JavaScript
+
+const campoSenha = document.getElementById("senha");
+const btnSenha = document.querySelector("#mostrar-senha");
+
+
+
+btnSenha.addEventListener("click", function() {
+    
+    //if(campoSenha.type == "password") {
+    //    campoSenha.type ="text";
+    //}
+
+    //else {
+    //    campoSenha.type = "password";
+    //}
+
+    campoSenha.type = campoSenha.type == "password" ? "text" : "password"
+
+    
+})
